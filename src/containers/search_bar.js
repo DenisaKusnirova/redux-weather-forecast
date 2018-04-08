07ad6c96a54a5e1aa2504d6094cde44d3affda02
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-04-08 21:40:36 +0200

    Turn the input into a controlled component and implement onInputChange function

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
-export default class Searchbar extends Component {
+export default class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            term: ''
+        }
+
+        // this which is an instance of SearchBar has a function called onInputChange
+        // bind that function to this (which is SearchBar) and then replace
+        // onInputChange with this new bind instance of this function
+        // or take the existing function, bind it to this and then replace the existing
+        // function with this
+        this.onInputChange = this.onInputChange.bind(this)
+    }
+
+    onInputChange(event) {
+        this.setState({term: event.target.value})
+    }
+
     render() {
         return (
             <form className="input-group">
-                <input />
+                <input 
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
